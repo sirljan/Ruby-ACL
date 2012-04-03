dd237c137c16eedcb6e8c9802fd57d0cd7f6d698
sirljan <jan.sirl@seznam.cz>
2012-04-03 23:04:39 +0200

    Removed all old files.

--- Src/Ruby-ACL/lib/accessor.rb
+++
@@ -1,6 +0,0 @@
-class Accessor < Principal     #user, proces or whoever is accessing resource objects
-  #mel by byt Is A Child Principal stejne jako Group
-  def initialize(id, name, member_of)
-    super(id, name, member_of)   #initialize of ancestor Principal
-  end
-end
